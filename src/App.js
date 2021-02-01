3073d7d1f5c38cebd1dc8518bd82c7db00736911
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-02-01 10:12:57 +0800

    Segregate components into separate routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import "./App.css";
 import Filter from "./components/Filter";
 import Counter from "./components/Counter";
 import TodoList from "./components/TodoList";
+import { BrowserRouter, NavLink, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>React testing library</h1>
-      <Filter />
-      <Counter />
-      <TodoList />
+      <BrowserRouter>
+        <NavLink to="/counter">Counter</NavLink>
+        <NavLink to="/filter">Filter</NavLink>
+        <NavLink to="/todolist">TodoList</NavLink>
+
+        <Route path="/counter" component={Counter} />
+        <Route path="/filter" component={Filter} />
+        <Route path="/todolist" component={TodoList} />
+      </BrowserRouter>
     </div>
   );
 }
